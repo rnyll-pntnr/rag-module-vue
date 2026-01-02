1a4a49d71b62d93a44fde986f6d175c9b762be98
BWCDEV1\bwcde <ranyll@bwc.ae>
2026-01-02 17:38:17 +0400

    feat: update route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\RAG\Http\Controllers\RAGController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('rags', RAGController::class)->names('rag');
+    Route::prefix('dashboard')->group(function () {
+        Route::resource('rags', RAGController::class)->names('rag');
+    });
 });
